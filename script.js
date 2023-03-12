2448f8a489d94bda1df0a0051450c89fa2ceaa9c
Mario <mario@supermario.dk>
2023-03-12 13:38:06 +0100

    commit 3

--- script.js
+++ script.js
@@ -5,23 +5,23 @@ const SearchedCity =  document.getElementById("city");
 const FeelsLike    =  document.getElementById("FeelsLikeTemp");
 const Temprature   =  document.getElementById("Temp");
 const CurrentTime  =  document.getElementById("CurrentTime");
-const WindSpeed    =  document.getElementById("WindSpeed");
+const WindSpeed    =  document.getElementById("Windspeed");
 const Humidity     =  document.getElementById("Humidity");
 const weather      =  document.getElementById("Weather");
 const WeatherCondition = document.getElementById("Weather-description")
 
-const ImagesLocation = ["Images/FeelsLike.png","Images/temperature.svg","Images/Humidity.png"];
+const ImagesLocation = ["Images/FeelsLike.png","Images/temperature.svg","Images/Humidity.png","Images/wind-blow.png"];
 const Images = [];
 
 let API = "https://api.openweathermap.org/data/2.5/weather?q=&appid=db22b0db45514e9d3d4ebd192516153e";
 //Adds an eventListener when the user clicks the search button, the api wil search for a city in the ShorAPIdata function
 SearchButton.addEventListener("click",ShowAPIdata);
 
-for(let i = 0; i<3; i++){
+for(let i = 0; i<4; i++){
     const img = document.createElement("img")
     img.src = ImagesLocation[i]
-    img.style.width = "25px"
-    img.style.height = "25px"
+    img.style.width = "30px"
+    img.style.height = "30px"
     Images.push(img)
 }
 
@@ -50,6 +50,7 @@ function ShowAPIdata(){
             if(Temprature.classList.contains("weather-info")){
             weather.classList.toggle('expanded')
             weather.addEventListener('transitionend',ShowData)
+            console.log(data.wind.speed)
             }
 
             else{
@@ -76,15 +77,18 @@ function ShowAPIdata(){
 
             }
             FeelsLike.innerText = `Feels like: ${main.feels_like}°C`;
-            Temprature.innerText = `Temprature: ${main.temp}°C`;
+            Temprature.innerText = `${main.temp}°C`;
             Humidity.innerText = `Humidity: ${main.humidity}%`;
+            WindSpeed.innerText = `${data.wind.speed}KP/H`
             Temprature.appendChild(Images[1]);
             Humidity.appendChild(Images[2]);
             FeelsLike.appendChild(Images[0]);
+            WindSpeed.appendChild(Images[3]);
             SearchedCity.innerHTML = searchValue;
             FeelsLike.classList.remove("weather-info");
             Temprature.classList.remove("weather-info");
             Humidity.classList.remove("weather-info");
+            WindSpeed.classList.remove("weather-info")
             
             }
         })
